2061a5162b6b4367761d3e3bbaafd8bad8fa1399
Manojit Das <manojitdas236@gmail.com>
2025-10-21 08:03:36 +0530

    Fix hero image imports for GitHub Pages

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -3,15 +3,14 @@ import React, { useState, useEffect } from 'react';
 import './hero.css';
 import Arrow from '../assets/arrow.png';
 import Brochure from '../assets/BROCHURE.pdf'
+import hero1 from '../assets/hero_1.jpg';
+import hero2 from '../assets/hero_2.jpg';
+import hero3 from '../assets/hero_3.jpeg';
+import hero4 from '../assets/hero_4.JPEG';
+import hero5 from '../assets/hero_5.jpg';
 
 
-const images = [
-  '/hero1.jpg',
-  '/hero2.jpg',
-  '/hero3.jpg',
-  '/hero4.jpg',
-  '/hero5.jpg'
-];
+const images = [hero1, hero2, hero3, hero4, hero5];
 
 const Hero = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
