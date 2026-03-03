ae69e079d308e5bdb4b4e6b4720e13eff07b81fd
Manojit Das <manojitdas236@gmail.com>
2026-03-03 11:46:52 +0530

    fixed vite base path for vercel

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/SUBHO-S-COMPUTER-INSTITUTE-/', // 👈 important for GitHub Pages
+  base: '/', // 👈 important for GitHub Pages
 })
